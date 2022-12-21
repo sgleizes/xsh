89da0e3dc1abaa31c8d6413aab0faf4e166c5092
sgleizes <stephane.gleizes@gmail.com>
2022-12-21 23:50:55 +0100

    Bump version to 0.4.1

--- xsh.sh
+++ xsh.sh
@@ -27,7 +27,7 @@
 #
 
 # shellcheck shell=sh disable=SC1090 disable=SC1007 disable=SC3043
-XSH_VERSION='0.4.0'
+XSH_VERSION='0.4.1'
 
 # Figure out the name of the current shell.
 XSHELL="${ZSH_NAME:-${0##*/}}"
